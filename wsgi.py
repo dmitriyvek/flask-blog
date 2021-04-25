d12cb486f25981a0f079ea2af6d58e75b7d90228
dmitriyvek <dmitriyvek@mail.ru>
2021-04-25 11:29:05 +0000

    fixing .evn loading

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ from flask_blog.blog import models as blog_models
 from flask_blog.users import models as user_models
 
 
-# load_dotenv()
+load_dotenv()
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
